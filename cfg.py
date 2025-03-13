11f7497717456fd13742154e43129116eec305bb
Yurii Roziaiev <yurii.roziaeiv@leadgence.com>
2025-03-13 22:29:45 +0200

    Added generation of similar files for source image

--- cfg.py
+++ cfg.py
@@ -7,16 +7,29 @@ Written by Yu Qian
 
 # dir
 data_dir = 'Synthtext\\files\\data'
-i_t_dir = 'i_t'
-i_s_dir = 'i_s'
-t_sk_dir = 't_sk'
-t_t_dir = 't_t'
-t_b_dir = 't_b'
-t_f_dir = 't_f'
-mask_t_dir = 'mask_t'
+
+bg_img_dir = "bg_img"
+
+source_text_dir = "source_text"
+target_text_dir = "target_text"
+
+source_img_dir = "source_img"
+target_img_dir = "target_img"
+
+source_img_grey_dir = "source_img_grey"
+target_img_grey_dir = "target_img_grey"
+
+source_img_styled_dir = "source_img_styled"
+target_img_styled_dir = "target_img_styled"
+
+source_sk_dir = "source_sk"
+target_sk_dir = "target_sk"
+
+source_mask_dir = "source_mask"
+target_mask_dir = "target_mask"
 
 # sample
-sample_num = 25000
+sample_num = 20
 
 # multiprocess
 process_num = 16
--- datagen.py
+++ datagen.py
@@ -17,43 +17,82 @@ def makedirs(path):
 
 
 def main():
-    
-    i_t_dir = os.path.join(cfg.data_dir, cfg.i_t_dir)
-    i_s_dir = os.path.join(cfg.data_dir, cfg.i_s_dir)
-    t_sk_dir = os.path.join(cfg.data_dir, cfg.t_sk_dir)
-    t_t_dir = os.path.join(cfg.data_dir, cfg.t_t_dir)
-    t_b_dir = os.path.join(cfg.data_dir, cfg.t_b_dir)
-    t_f_dir = os.path.join(cfg.data_dir, cfg.t_f_dir)
-    mask_t_dir = os.path.join(cfg.data_dir, cfg.mask_t_dir)
-    
-    makedirs(i_t_dir)
-    makedirs(i_s_dir)
-    makedirs(t_sk_dir)
-    makedirs(t_t_dir)
-    makedirs(t_b_dir)
-    makedirs(t_f_dir)
-    makedirs(mask_t_dir)
+
+    bg_img_dir = os.path.join(cfg.data_dir, cfg.bg_img_dir)
+    source_text_dir = os.path.join(cfg.data_dir, cfg.source_text_dir)
+    target_text_dir = os.path.join(cfg.data_dir, cfg.target_text_dir)
+    source_img_dir = os.path.join(cfg.data_dir, cfg.source_img_dir)
+    target_img_dir = os.path.join(cfg.data_dir, cfg.target_img_dir)
+    source_img_grey_dir = os.path.join(cfg.data_dir, cfg.source_img_grey_dir)
+    target_img_grey_dir = os.path.join(cfg.data_dir, cfg.target_img_grey_dir)
+    source_img_styled_dir = os.path.join(cfg.data_dir, cfg.source_img_styled_dir)
+    target_img_styled_dir = os.path.join(cfg.data_dir, cfg.target_img_styled_dir)
+    source_sk_dir = os.path.join(cfg.data_dir, cfg.source_sk_dir)
+    target_sk_dir = os.path.join(cfg.data_dir, cfg.target_sk_dir)
+    source_mask_dir = os.path.join(cfg.data_dir, cfg.source_mask_dir)
+    target_mask_dir = os.path.join(cfg.data_dir, cfg.target_mask_dir)
+
+    makedirs(bg_img_dir)
+    makedirs(source_text_dir)
+    makedirs(target_text_dir)
+    makedirs(source_img_dir)
+    makedirs(target_img_dir)
+    makedirs(source_img_grey_dir)
+    makedirs(target_img_grey_dir)
+    makedirs(source_img_styled_dir)
+    makedirs(target_img_styled_dir)
+    makedirs(source_sk_dir)
+    makedirs(target_sk_dir)
+    makedirs(source_mask_dir)
+    makedirs(target_mask_dir)
 
     mp_gen = multiprocess_datagen(cfg.process_num, cfg.data_capacity)
     mp_gen.multiprocess_runningqueue()
     digit_num = len(str(cfg.sample_num)) - 1
     for idx in range(cfg.sample_num):
         print("Generating step {:>6d} / {:>6d}".format(idx + 1, cfg.sample_num))
-        i_t, i_s, t_sk, t_t, t_b, t_f, mask_t = mp_gen.dequeue_data()
-        i_t_path = os.path.join(i_t_dir, str(idx).zfill(digit_num) + '.png')
-        i_s_path = os.path.join(i_s_dir, str(idx).zfill(digit_num) + '.png')
-        t_sk_path = os.path.join(t_sk_dir, str(idx).zfill(digit_num) + '.png')
-        t_t_path = os.path.join(t_t_dir, str(idx).zfill(digit_num) + '.png')
-        t_b_path = os.path.join(t_b_dir, str(idx).zfill(digit_num) + '.png')
-        t_f_path = os.path.join(t_f_dir, str(idx).zfill(digit_num) + '.png')
-        mask_t_path = os.path.join(cfg.data_dir, cfg.mask_t_dir, str(idx).zfill(digit_num) + '.png')
-        cv2.imwrite(i_t_path, i_t, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
-        cv2.imwrite(i_s_path, i_s, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
-        cv2.imwrite(t_sk_path, t_sk, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
-        cv2.imwrite(t_t_path, t_t, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
-        cv2.imwrite(t_b_path, t_b, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
-        cv2.imwrite(t_f_path, t_f, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
-        cv2.imwrite(mask_t_path, mask_t, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+        (
+            bg_img,
+            source_text, target_text,
+            source_img, target_img,
+            source_img_grey, target_img_grey,
+            source_img_styled, target_img_styled,
+            source_sk, target_sk,
+            source_mask, target_mask
+        ) = mp_gen.dequeue_data()
+
+        bg_img_path = os.path.join(bg_img_dir, str(idx).zfill(digit_num) + '.png')
+        source_text_path = os.path.join(source_text_dir, str(idx).zfill(digit_num) + '.png')
+        target_text_path = os.path.join(target_text_dir, str(idx).zfill(digit_num) + '.png')
+        source_img_path = os.path.join(source_img_dir, str(idx).zfill(digit_num) + '.png')
+        target_img_path = os.path.join(target_img_dir, str(idx).zfill(digit_num) + '.png')
+        source_img_grey_path = os.path.join(source_img_grey_dir, str(idx).zfill(digit_num) + '.png')
+        target_img_grey_path = os.path.join(target_img_grey_dir, str(idx).zfill(digit_num) + '.png')
+        source_img_styled_path = os.path.join(source_img_styled_dir, str(idx).zfill(digit_num) + '.png')
+        target_img_styled_path = os.path.join(target_img_styled_dir, str(idx).zfill(digit_num) + '.png')
+        source_sk_path = os.path.join(source_sk_dir, str(idx).zfill(digit_num) + '.png')
+        target_sk_path = os.path.join(target_sk_dir, str(idx).zfill(digit_num) + '.png')
+        source_mask_path = os.path.join(source_mask_dir, str(idx).zfill(digit_num) + '.png')
+        target_mask_path = os.path.join(target_mask_dir, str(idx).zfill(digit_num) + '.png')
+
+
+        cv2.imwrite(bg_img_path, bg_img, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+        print(source_text, target_text)
+
+        cv2.imwrite(source_img_path, source_img, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+        cv2.imwrite(target_img_path, target_img, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+
+        cv2.imwrite(source_img_grey_path, source_img_grey, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+        cv2.imwrite(target_img_grey_path, target_img_grey, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+
+        cv2.imwrite(source_img_styled_path, source_img_styled, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+        cv2.imwrite(target_img_styled_path, target_img_styled, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+
+        cv2.imwrite(source_sk_path, source_sk, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+        cv2.imwrite(target_sk_path, target_sk, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+
+        cv2.imwrite(source_mask_path, source_mask, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+        cv2.imwrite(target_mask_path, target_mask, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
 
     mp_gen.terminate_pool()
 
